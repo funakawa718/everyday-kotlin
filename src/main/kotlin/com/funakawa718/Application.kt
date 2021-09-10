7cef3be75b307c7d0735ef7c2187283ea7f6fd18
funakawa718 <taiki.funakawa@shelfy.jp>
2021-09-11 00:05:32 +0900

    hello world

---
+++ src/main/kotlin/com/funakawa718/Application.kt
@@ -0,0 +1,12 @@
+package com.funakawa718
+
+import io.ktor.application.*
+import com.funakawa718.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
